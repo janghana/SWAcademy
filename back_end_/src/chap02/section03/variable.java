4df8cef938d774e3eadc428d25fe4c7a6f824581
janghana <han001112@icloud.com>
2023-01-02 10:36:39 +0900

    upload chap02

---
+++ back_end_/src/chap02/section03/variable.java
@@ -0,0 +1,11 @@
+package chap02.section03;
+
+public class variable {
+    public static void main(String[] args) {
+        int age = 17;
+        int salary = 5000;
+
+        System.out.println(age);
+        System.out.println(age + salary);
+    }
+}
