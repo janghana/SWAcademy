8dab336b57fd1bf14903893e3ddff6360d260b45
janghana <han001112@icloud.com>
2023-01-03 09:02:00 +0900

    upload for_interim_evaluation_1.py

--- coding_test/for_interim_evaluation/for_interim_evaluation_1.py
+++ coding_test/for_interim_evaluation/for_interim_evaluation_1.py
@@ -1,20 +1,11 @@
-arr1 = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
+T = int(input())
 
-tmp_arr = [[] for _ in range(len(arr1)+len(arr2)+3)]
-for i in range(len(arr1)):
-    tmp_arr[i*2] = arr1[i]
-for i in range(len(arr2)):
-    tmp_arr[2*i + 1] = arr2[i]
-
-result_char = ""
-for i in tmp_arr:
-    if isinstance(i, list):
-        continue
-    result_char += str(i)+" "
-
-print(result_char)
-'''
-1 3 4 9 3 1 2 8 4 8
-2 9 37 1 39 4 58 60
-'''
+for _ in range(T):
+    N = int(input())
+    N_list = list(map(int, input().split()))
+    count = int(input())
+    for i in range(count):
+        U, V = map(int, input().split())
+        N_list[U - 1] -= 1
+        N_list[V - 1] += 1
+    print(*N_list)
