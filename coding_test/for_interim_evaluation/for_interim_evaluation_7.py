d797a6a186ecff7bfaf456a0f84c36b0554293a2
janghana <han001112@icloud.com>
2023-01-05 09:29:19 +0900

    upload for_interim_evaluation_8.py

--- coding_test/for_interim_evaluation/for_interim_evaluation_7.py
+++ coding_test/for_interim_evaluation/for_interim_evaluation_7.py
@@ -1,22 +1,5 @@
-N = int(input())
-x_y_list = []
-for _ in range(N):
-    x_y_list.append(list(map(int,input().split())))
-result = []
-x_0, y_0 = x_y_list[0]
-print(x_y_list)
-for x,y in x_y_list[1:]:
-    cnt = 0
-    if x == x_0 and y != y_0:
-        continue
-    a = (y-y_0)/(x-x_0)
-    b = y - (x * a)
-    print(a,b)
-    for x1,y1 in x_y_list:
-        if y1 == a * x1 + b:
-            cnt += 1
-    result.append(cnt)
-if len(result) == 0:
-    print(1)
+from collections import Counter
+if Counter(input())['#'] > 1:
+    print("HELP!")
 else:
-    print(max(result))
+    print("HAHA!")
